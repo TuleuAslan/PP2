2f7deb07042dd9895981113c666af5db3b631ca2
TuleuAslan <47240546+TuleuAslan@users.noreply.github.com>
2019-02-01 16:33:23 +0600

    Add files via upload

---
+++ Week 1/Task 1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example8
+{
+    class Program
+    {
+        static bool Prime(int n) //boolean function which checks is it prime or no
+        {
+            bool b = true;
+            if (n <= 1)
+            {
+                b = false;
+                return b;
+            }
+            for(int i=2;i<n;i++)
+            {
+                if(n % i == 0)
+                {
+                    b = false;
+                    return b;
+                }
+            }
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Converting String to Integer
+            int[] ar = new int[n];             //Creating Array with n Length
+            string s = Console.ReadLine();    // Writing elements of array
+            string[] ss = s.Split(' ');     //adding elements to array of type string 
+            for(int i=0;i<ss.Length;++i)    //converting string elements to integer and putting them to integer array
+            {
+                ar[i] = int.Parse(ss[i]);
+            }
+            List<int> b = new List<int>(); // creating something like vector
+            foreach(int q in ar) // checking each element to prime
+            {
+                if(Prime(q))
+                {
+                    b.Add(q);
+                }
+            }
+            Console.WriteLine(b.ToArray().Length); // Showing lenght of prime numbers
+            for(int i=0;i< b.ToArray().Length;++i) //showing prime numbers by space
+            {
+                Console.Write(b[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Week 1/Task 3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //Giving length of array
+            string s = Console.ReadLine(); //writing elements of array
+            string[] ss = s.Split(' '); //adding elements to string array by space
+            int[] arr1 = new int[n]; // creating array with n length
+            for(int i = 0; i < ss.Length; i++) // converting each element of string array to integer element
+            {
+                arr1[i] = int.Parse(ss[i]);
+            }
+            foreach(int q in arr1) // showing each element twice
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    Console.Write(q+" ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
