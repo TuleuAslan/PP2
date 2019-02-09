400661719164e1680f6401314fa6dcbd9f9b0391
TuleuAslan <47240546+TuleuAslan@users.noreply.github.com>
2019-02-09 09:58:06 +0600

    Add files via upload

---
+++ Week 2/NewTask4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace NewTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = @"C:\Users\пк\Desktop\PP2 C#\Week 2\NewTask4\first.txt";
+            StreamWriter wr = new StreamWriter(a);
+            wr.Write("Kobiii");
+            wr.Close();
+            string b = @"C:\Users\пк\Desktop\PP2 C#\Week 2\NewTask4\second.txt";
+            File.Copy(a, b, true);
+            File.Delete(a);
+        }
+    }
+}
---
+++ Week 2/NewTask4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("NewTask4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("NewTask4")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3d31fa2b-b5a6-41a3-9b90-8bd2d44d8a48")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
